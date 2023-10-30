d2ad49f0c022691a286a9730858f6feaa766f648
John Christian Balang <dev.jcbalang@gmail.com>
2023-10-30 15:06:21 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Test site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms"]
+};
